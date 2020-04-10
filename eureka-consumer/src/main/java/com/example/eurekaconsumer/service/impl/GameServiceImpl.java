0e9e07f0d307ff1b7beb057cc449612f67266f28
u7e2b <2774350139@qq.com>
2020-04-10 09:55:51 -0700

    3

--- eureka-consumer/src/main/java/com/example/eurekaconsumer/service/impl/GameServiceImpl.java
+++ eureka-consumer/src/main/java/com/example/eurekaconsumer/service/impl/GameServiceImpl.java
@@ -75,90 +75,52 @@ public class GameServiceImpl implements GameService {
         }
         return cols;
     }
-
-    /**
-     * 给用户添加游戏购买记录
-     * @return
-     */
+    
     @Override
     public void insertPurchaseRecord(PurchaseRecordEntity purchaseRecordEntity){
         GameDao.insertPurchaseRecord(purchaseRecordEntity);
     }
 
-    /**
-     * 给用户添加游戏
-     * @return
-     */
     @Override
     public void insertPurchase(PurchaseEntity purchaseEntity){
         GameDao.insertPurchase(purchaseEntity);
     }
 
-    /**
-     * 删除用户游戏（保留用户购买记录，因为购买记录比较重要不建议删除）
-     * @return
-     */
     @Override
     public void deletePubchaseRecord(String gameId){
         GameDao.deletePubchaseRecord(gameId);
     }
 
-    /**
-     * 添加用户
-     * @return
-     */
     @Override
     public void insertUserAccount(UserAccountEntity userAccount){
         GameDao.insertUserAccount(userAccount);
     }
 
-    /**
-     * 删除用户
-     */
     @Override
     public void deleteUserAccount(String accountID){
         GameDao.deleteUserAccount(accountID);
     }
 
-    /**
-     * 查看所有用户
-     * @return
-     */
     @Override
     public List<UserAccountEntity> selectUserAccountAll(){
         return GameDao.selectUserAccountAll();
     }
 
-    /**
-     * 查看所有会员列表
-     * @return
-     */
     @Override
     public List<JoinedMembershipEntity> selectMembershipAll(){
         return GameDao.selectMembershipAll();
     }
 
-    /**
-     * 根据用户名查询
-     * @return
-     */
     @Override
     public UserAccountEntity selectUserAccount(String accountName){
         return GameDao.selectUserAccount(accountName);
     }
 
-    /**
-     * 聚合操作
-     * @return
-     */
     @Override
     public List<UserAccountEntity> selectUserByAggregationTpye(String aggregationTpye){
         return GameDao.selectUserByAggregationTpye(aggregationTpye);
     }
 
-    /**
-     * 删除用户
-     */
     @Override
     public void deldteUserAccount(String acconutId) {
         GameDao.deldteUserAccount(acconutId);
