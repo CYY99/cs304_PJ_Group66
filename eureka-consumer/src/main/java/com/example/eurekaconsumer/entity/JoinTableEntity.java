d7506631af9d23cf0ce42edc967e93a43bb77fec
u7e2b <2774350139@qq.com>
2020-04-10 02:06:02 -0700

    2

---
+++ eureka-consumer/src/main/java/com/example/eurekaconsumer/entity/JoinTableEntity.java
@@ -0,0 +1,231 @@
+package com.example.eurekaconsumer.entity;
+
+import java.util.Date;
+
+public class JoinTableEntity {
+    private  String gameID;
+    private  String gameType;
+    private  String gName;
+    private  int ageLimit;
+    private  double price;
+    private Date publishDate;
+    private  String developerID;
+    private  String membershipID;
+    private  String status;
+    private  double feePerMonth;
+    private  Date dataJoined;
+    private  String accountID;
+    private String developerName;
+    private String accountId;
+    private String gender;
+    private Date registerDate;
+    private Date birthDate;
+    private int age;
+    private String accountName;
+    private String count;
+    private String gameId;
+    private String acconutId;
+    private int max;
+    private int min;
+    private String type;
+
+    public String getGameID() {
+        return gameID;
+    }
+
+    public void setGameID(String gameID) {
+        this.gameID = gameID;
+    }
+
+    public String getGameType() {
+        return gameType;
+    }
+
+    public void setGameType(String gameType) {
+        this.gameType = gameType;
+    }
+
+    public String getgName() {
+        return gName;
+    }
+
+    public void setgName(String gName) {
+        this.gName = gName;
+    }
+
+    public int getAgeLimit() {
+        return ageLimit;
+    }
+
+    public void setAgeLimit(int ageLimit) {
+        this.ageLimit = ageLimit;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public String getDeveloperID() {
+        return developerID;
+    }
+
+    public void setDeveloperID(String developerID) {
+        this.developerID = developerID;
+    }
+
+    public String getMembershipID() {
+        return membershipID;
+    }
+
+    public void setMembershipID(String membershipID) {
+        this.membershipID = membershipID;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getFeePerMonth() {
+        return feePerMonth;
+    }
+
+    public void setFeePerMonth(double feePerMonth) {
+        this.feePerMonth = feePerMonth;
+    }
+
+    public Date getDataJoined() {
+        return dataJoined;
+    }
+
+    public void setDataJoined(Date dataJoined) {
+        this.dataJoined = dataJoined;
+    }
+
+    public String getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(String accountID) {
+        this.accountID = accountID;
+    }
+
+    public String getDeveloperName() {
+        return developerName;
+    }
+
+    public void setDeveloperName(String developerName) {
+        this.developerName = developerName;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(Date registerDate) {
+        this.registerDate = registerDate;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+    public String getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(String gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getAcconutId() {
+        return acconutId;
+    }
+
+    public void setAcconutId(String acconutId) {
+        this.acconutId = acconutId;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
